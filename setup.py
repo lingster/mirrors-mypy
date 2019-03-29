6111d32e8fcfc50aebf01ca3f6f2b40dbfd0bd05
Ling Li <lingster@users.noreply.github.com>
2019-03-29 09:59:18 +0000

    use specific stub version

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup
 setup(
     name='pre_commit_dummy_package',
     version='0.0.0',
-    install_requires=['mypy==0.670', 'django-stubs', 'djangorestframework-stubs'],
+    install_requires=['mypy==0.670', 'django-stubs==0.10.2', 'djangorestframework-stubs==0.4.2'],
 )
